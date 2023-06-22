481adbdfca5e1f784f27d7099496032c78bde4cc
POOJARV-90 <132448302+POOJARV-90@users.noreply.github.com>
2023-06-21 22:41:23 -0700

    Update majorityelement169.js

--- majorityelement169.js
+++ majorityelement169.js
@@ -10,24 +10,36 @@
 
 
 
+// var majorityElement = function(nums) {
+//   let majority = nums[0];
+//   let votes = 1;
+
+//   for (let i = 1; i < nums.length; i++) {
+//     if (votes === 0) {
+//       votes++;
+//       majority = nums[i];
+//     } else if (majority === nums[i]) {
+//       votes++;
+//     } else {
+//       votes--;
+//     }
+//   }
+
+//   return majority;
+// };
+
+
+// console.log(majorityElement(nums)); 
+
+// ---------------------------------------------------------------
+
+
 var majorityElement = function(nums) {
-  let majority = nums[0];
-  let votes = 1;
-
-  for (let i = 1; i < nums.length; i++) {
-    if (votes === 0) {
-      votes++;
-      majority = nums[i];
-    } else if (majority === nums[i]) {
-      votes++;
-    } else {
-      votes--;
-    }
-  }
-
-  return majority;
+  nums.sort((a, b) => a - b);
+  return nums[Math.floor(nums.length / 2)];
 };
 
 
-console.log(majorityElement(nums)); 
+console.log(majorityElement(nums));  
+
 
