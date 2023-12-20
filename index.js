c0d8f172d4599ebb70642909ca1aa666f3a7f432
POOJARV-90 <132448302+POOJARV-90@users.noreply.github.com>
2023-12-20 02:38:47 -0800

    UPDATE

---
+++ index.js
@@ -0,0 +1,17 @@
+
+// 1 .
+// let qusetion = NaN === NaN
+// console.log(qusetion); //false
+
+// let que = 1>2>3
+// console.log(que); //false
+
+
+// let a = 5
+// let b = 6
+// console.log(++a + b++);//12
+//++a ===> pre-increment
+//b++ ===> post-increment
+
+
+
